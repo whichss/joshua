c0259798e798cecc73a3c7a2be0bb7bde363284f
Limhee <66983196+whichss@users.noreply.github.com>
2025-06-01 15:35:00 +0900

    Update typing-animation.js

--- js/typing-animation.js
+++ js/typing-animation.js
@@ -23,9 +23,9 @@ document.addEventListener('DOMContentLoaded', function() {
   const typing4 = document.getElementById('typing-4');
 
   if (typing1 && typing2 && typing3 && typing4) {
-    typeAnimation(typing1, 'Shema', 0);
-    typeAnimation(typing2, 'Israel', 800);
-    typeAnimation(typing3, '이스라엘아 들으라!', 1600);
+    typeAnimation(typing1, 'Shema Israel', 0);
+    typeAnimation(typing2, '이스라엘아 들으라!', 800);
+    typeAnimation(typing3, '', 1600);
     typeAnimation(typing4, '이스라엘아 들으라 우리 하나님 여호와는 오직 유일한 여호와이시니', 2400);
   }
 });
